44651acfe7620e9c5bfb35cae5abe000341db73d
Jordi <jordi@kodilab.com>
2021-06-13 19:32:11 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { CLI } from './cli';
 export { Command } from './command/command';
+export { CommandGroup } from './command/command-group';
+export { Routes } from './command/command-group';
 export { IOption } from './command/input/option';
 export { IArgument } from './command/input/argument';
+export { ArgumentBag, OptionBag } from './command/input/bag/bag';
 export { QuestionType } from './io/input-output.interface';
